9ee37fefb4efd35ddce2821c40b224a5fdcdd1e3
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-07-15 19:27:52 -0700

    Added basic methods to doubly linked list

--- DoubleyLinkedList.cpp
+++ DoubleyLinkedList.cpp
@@ -2,3 +2,23 @@
 #include "DoublyNode.h"
 #include "DoublyLinkedList.h"
 
+// O(1)
+int DoublyLinkedList::Length() {
+    return length;
+};
+
+// O(1)
+int DoublyLinkedList::Peek() {
+    if (length == 0) {
+        return NULL;
+    };
+    return head->val;
+};
+
+// O(1)
+int DoublyLinkedList::Tail() {
+    if (length == 0) {
+        return NULL;
+    };
+    return tail->val;
+};
